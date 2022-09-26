f9a8c9a2ec7be59ad37c33c365f7a1befba06618
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-09-26 12:01:56 -0300

    Operadores Aritméticos

---
+++ BootcampDIO/Calculadora.cs
@@ -0,0 +1,27 @@
+using  BootcampDIO.Models;
+
+
+    namespace  calculadora
+    {
+        class  CalculadoraProgram
+        {
+            static  void  Main(string[] args)
+            {
+                // var  calculadora  =  new  Calculadora();
+                // calculadora.Somar(10, 5);
+                // calculadora.Subtrair(10, 5);
+                // calculadora.Multiplicar(10, 5);
+                // calculadora.Dividir(10, 5);
+                // calculadora.Potencia(10, 5);
+                // calculadora.Seno(10);
+                // calculadora.Cosseno(10);
+                // calculadora.Tangente(10);   
+                //calculadora.RaizQuadrada(10);
+
+                // int numero = 10;
+                // numero--; // ou ++ para incrementar
+                // Console.WriteLine(numero);
+
+            }
+        }
+    }
---
+++ BootcampDIO/Models/OperadoresAritmeticos.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampDIO.Models
+{
+   public class Calculadora 
+   {
+       public void Somar(int a, int b)
+       {
+           Console.WriteLine($"A soma de {a} + {b} = {a + b}");
+       }
+
+         public void Subtrair(int a, int b)
+         {
+              Console.WriteLine($"A subtração de {a} - {b} = {a - b}");
+         }
+
+            public void Multiplicar(int a, int b)
+            {
+                Console.WriteLine($"A multiplicação de {a} * {b} = {a * b}");
+            }
+            
+               public void Dividir(int a, int b)
+               {
+                   Console.WriteLine($"A divisão de {a} / {b} = {a / b}");
+               } 
+
+            public void Potencia(int a, int b)
+            {
+                Console.WriteLine($"A potência de {a} ^ {b} = {Math.Pow(a, b)}");
+            }
+
+            public void Seno(double a)
+            {
+                Console.WriteLine($"O seno de {a} = {Math.Sin(a)}");
+            }
+
+            public void Cosseno(int a)
+            {
+                Console.WriteLine($"O cosseno de {a} = {Math.Cos(a)}");
+            }
+
+            public void Tangente(int a)
+            {
+                Console.WriteLine($"A tangente de {a} = {Math.Tan(a)}");
+            }
+
+            public void RaizQuadrada(int a)
+            {
+                Console.WriteLine($"A raiz quadrada de {a} = {Math.Sqrt(a)}");
+            }
+   } 
+}
