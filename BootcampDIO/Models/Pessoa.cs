e115560912a0cedd9502909b0f5a737b4285d475
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-09-26 11:26:20 -0300

    Tipos de Operadores em C#

--- BootcampDIO/Models/Pessoa.cs
+++ BootcampDIO/Models/Pessoa.cs
@@ -3,16 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace BootcampDIO.Models
-{
-    public class Pessoa
-    {
-        public string PrimeiroNome { get; set; }
-        public int Idade { get; set; }
+// namespace BootcampDIO.Models
+// {
+//     public class Pessoa
+//     {
+//         public string PrimeiroNome { get; set; }
+//         public int Idade { get; set; }
 
-        public void Apresentar()
-        {
-            Console.WriteLine($"Olá, meu nome é {PrimeiroNome} e tenho {Idade} anos.");
-        }
-    }
-}
+//         public void Apresentar()
+//         {
+//             Console.WriteLine($"Olá, meu nome é {PrimeiroNome} e tenho {Idade} anos.");
+//         }
+
+    
+//     }
+// }
