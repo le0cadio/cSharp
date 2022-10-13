dbda7839e0b0cc028433f33574b9bfa293489a9c
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-10-13 12:59:11 -0300

    wip

---
+++ BootcampDIO/EstruturasDeRepeticao.cs
@@ -0,0 +1,24 @@
+int numero = 5;
+int contador = 0;
+
+
+while (contador <= 10)
+{
+   Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+   contador ++;
+}
+
+
+
+
+
+
+
+//laço de repetição FOR 
+
+// int numero = 2; 
+
+// for(int contador = 0; contador <= 10; contador++)
+// {
+//     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// }
