fb7d9f8fc93d01d229c1da6e2dd2d27cb0b8d0ad
gleocadi0 <guilhermeleocadio1301@gmail.com>
2022-10-17 14:20:34 -0300

    WIP bootcamp

---
+++ BootcampDIO/Basico/EstruturasDeRepeticao.cs
@@ -0,0 +1,91 @@
+// string opcao;
+// bool exibirMenu = true;
+
+// while (exibirMenu)
+// {
+//     Console.WriteLine("Digite a opção desejada: ");
+//     Console.WriteLine("1 - Cadastrar");
+//     Console.WriteLine("2 - Buscar cliente");
+//     Console.WriteLine("3 - Sair");
+//     opcao = Console.ReadLine();
+
+//     switch (opcao)
+//     {
+//         case "1":
+//             Console.WriteLine("Cadastrar");
+//             break;
+//         case "2":
+//             Console.WriteLine("Buscar cliente");
+//             break;
+//         case "3":
+//             Console.WriteLine("Sair");
+//             exibirMenu = false;
+//             break;
+//         default:
+//             Console.WriteLine("Opção inválida");
+//             break;
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // int soma = 0, numero = 0;
+
+// // do
+// // {
+// //     Console.WriteLine("Digite um número: (0 para parar)");
+// //     numero = Convert.ToInt32(Console.ReadLine());
+// //     soma += numero;
+// // } while (numero != 0);
+
+// // Console.WriteLine("A soma dos números digitados é: " + soma);
+
+
+
+
+
+
+
+
+
+// // int numero = 5;
+// // int contador = 0;
+
+
+// // while (contador <= 10)
+// // {
+// //    Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// //    contador ++;
+// // }
+
+
+
+
+
+
+
+// //laço de repetição FOR
+
+// // int numero = 2;
+
+// // for(int contador = 0; contador <= 10; contador++)
+// // {
+// //     Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+// // }
